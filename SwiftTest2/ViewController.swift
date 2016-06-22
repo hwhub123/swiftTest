c47cc4799089a30bc128c89c2b0298f48b75f0b3
F839-014 M <mf839-014@70-12-247-182.pools.spcsdns.net>
2016-06-22 17:18:25 +0900

    Initial Commit

---
+++ SwiftTest2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftTest2
+//
+//  Created by MF839-014 on 2016. 6. 22..
+//  Copyright © 2016년 MF839-014. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
